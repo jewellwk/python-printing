a8ab4e6f126db91341049ce72627707fe77fe8e2
codenamejewell <jewwri@gmail.com>
2024-09-04 09:22:11 -0400

    Create lab_3.py

---
+++ lab_3.py
@@ -0,0 +1,11 @@
+# Correct each print statement so the code executes successfully and outputs Hello World
+
+# Understanding Syntax
+print('Hello World")
+
+# Using Variables
+greeting = "Hello"
+print(Greeting, "World")
+
+# Using concatenation
+print("Hello"+"")
